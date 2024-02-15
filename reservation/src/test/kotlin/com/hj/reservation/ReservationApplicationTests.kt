8b923f6bf3ec60c9be657bc1c286c76bc5f6de71
HenJune <73812257+tkdlek0501@users.noreply.github.com>
2024-02-15 21:12:33 +0900

    init

---
+++ reservation/src/test/kotlin/com/hj/reservation/ReservationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hj.reservation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReservationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
