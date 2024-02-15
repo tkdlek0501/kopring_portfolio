cfe1683a15225a9f453ef10f50d870e444248124
kimhyunjun <tkdlek0501@naver.com>
2024-02-15 22:08:36 +0900

    store 엔티티 생성 / 작업중

---
+++ reservation/src/main/kotlin/com/hj/reservation/domain/store/Store.kt
@@ -0,0 +1,42 @@
+package com.hj.reservation.domain.store
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+// TODO: BaseEntity(@MappedSuperclass)
+@Entity
+class Store(
+    val name: String,
+
+    val phoneNumber: String,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+) {
+
+    init {
+        if (name.isBlank()) {
+            throw IllegalArgumentException("이름은 비어 있을 수 없습니다.")
+        }
+        // TODO: 정규화 등 validation
+    }
+
+    companion object {
+
+        // Object Model 패턴
+        fun fixture(
+            name: String = "매장 이름",
+            phoneNumber: String = "01000000000",
+            id: Long? = null,
+        ): Store {
+            return Store(
+                name = name,
+                phoneNumber = phoneNumber,
+                id = id,
+            )
+        }
+    }
+}
